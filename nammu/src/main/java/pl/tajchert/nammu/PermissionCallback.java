ec83018949db17ad91f3a2e2a8377ee94c9afe98
Houssem Zaier <houssem.zaier@gmail.com>
2018-11-20 16:31:20 +0100

    Update PermissionCallback.java

--- nammu/src/main/java/pl/tajchert/nammu/PermissionCallback.java
+++ nammu/src/main/java/pl/tajchert/nammu/PermissionCallback.java
@@ -4,6 +4,6 @@ package pl.tajchert.nammu;
  * Created by Michal Tajchert on 2015-06-04.
  */
 public interface PermissionCallback {
-    public void permissionGranted();
-    public void permissionRefused();
+    public void onPermissionGranted();
+    public void onPermissionRefused();
 }
